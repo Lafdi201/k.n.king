8a8286b3b8d1fbdb7e6c02c91649059d9e74f9ed
Lafdi201 <ziyad.lafdi@ensam.ac.ma>
2024-03-01 16:47:34 +0100

    A program that declares several unintialized int and float variables. Conclusion: there's no pattern to the values.

---
+++ qst4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+  int a,b,c,d;
+  float t,x,y,z;
+  printf("%d\n",a);
+  printf("%d\n",b);
+  printf("%d\n",c);
+  printf("%d\n",d);
+  printf("%f\n",t);
+  printf("%f\n",x);
+  printf("%f\n",y);
+  printf("%f\n",z);
+  return 0;
+}
