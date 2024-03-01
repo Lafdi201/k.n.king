b0454d9918c4c293bedeb1b1ff93586fd0bf7549
Lafdi201 <ziyad.lafdi@ensam.ac.ma>
2024-03-01 16:33:28 +0100

    condensing the program by initializing height, length ad width variable, and calculating the weight without directly.

--- dweight.c
+++ dweight.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
-  int height, length, width, volume, weight;
-  printf("Enter height of box: ");
-  scanf("%d", &height);
-  printf("Enter length of box: ");
-  scanf("%d", &length);
-  printf("Enter width of box: ");
-  scanf("%d", &width);
+  int height=10, length=8, width=5, volume;
+
   volume = height * length * width;
-  weight = (volume + 165) / 166;
+  printf("Dimensions: %dx%dx%d\n", length, width, height);
   printf("Volume (cubic inches): %d\n", volume);
-  printf("Dimensional weight (pounds): %d\n", weight);
+  printf("Dimensional weight (pounds): %d\n", (volume + 165) / 166);
   return 0;
 }
